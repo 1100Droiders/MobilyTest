97e7e3359e4b82fb0ae393932b101ed71c67b718
iYazee6 <iYazee6@gmail.com>
2014-06-23 18:02:25 +0300

    Toast .......

--- Tsbeeh/src/com/mobily/tsbeeh/MainActivity.java
+++ Tsbeeh/src/com/mobily/tsbeeh/MainActivity.java
@@ -2,8 +2,7 @@ package com.mobily.tsbeeh;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		Toast.makeText(MainActivity.this , " ", Toast.LENGTH_SHORT).show();
 	}
 }
